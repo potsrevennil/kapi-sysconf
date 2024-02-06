1d726bf65a33751e27d6df7e72e4439c1537bfab
Thing-han, Lim <b029011144@gmail.com>
2024-02-06 14:47:09 +0800

    refactor(darwin): allow unfree and format the darwin nix file

--- darwin.nix
+++ darwin.nix
@@ -1,34 +1,57 @@
 { pkgs, lib, ... }:
 {
-  nix.settings.auto-optimise-store = true;
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    settings = {
+      auto-optimise-store = true;
+      trusted-users = [
+        "root"
+        "thing-hanlim"
+      ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   programs.zsh.enable = true;
-  environment.shells = [ pkgs.bash pkgs.zsh ];
-  environment.loginShell = pkgs.zsh;
-  environment.variables = {
-    EDITOR = "vim";
+
+  environment = {
+    shells = with pkgs; [ bash zsh ];
+    loginShell = pkgs.zsh;
+    variables = {
+      EDITOR = "vim";
+    };
+    systemPackages = with pkgs; [
+      home-manager
+    ];
   };
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
+
   services.nix-daemon.enable = true;
-  system.stateVersion = 4;
-  system.defaults = {
-    finder = {
-      AppleShowAllExtensions = true;
-      _FXShowPosixPathInTitle = true;
+
+  system = {
+    stateVersion = 4;
+    defaults = {
+      finder = {
+        AppleShowAllExtensions = true;
+        _FXShowPosixPathInTitle = true;
+      };
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        InitialKeyRepeat = 14;
+        KeyRepeat = 2;
+      };
+      dock.autohide = true;
     };
-    NSGlobalDomain = {
-      AppleShowAllExtensions = true;
-      InitialKeyRepeat = 14;
-      KeyRepeat = 2;
-    };
-    dock.autohide = true;
   };
+
   fonts = {
     fontDir.enable = true;
     fonts = with pkgs; [ (nerdfonts.override { fonts = [ "Agave" ]; }) ];
   };
+
   homebrew = {
     enable = true;
     caskArgs.no_quarantine = true;
