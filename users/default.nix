9f6242f619e5f73e4d5cadb4f85afd18f7e96d52
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-10-30 03:35:56 +0800

    refactor: remove the unnecessary nix module interface for home-manager
    Add support for home-manager lite dependencies

--- users/default.nix
+++ users/default.nix
@@ -1,21 +1,25 @@
-_:
+{ inputs, withSystem, ... }:
 {
-  homeConfigurations =
-    {
-      "thing-hanlim@Thing-hans-MacBook-Pro" = {
-        system = "aarch64-darwin";
-        stateVersion = "24.05";
+  config.flake.homeConfigurations = {
+    "thing-hanlim@Thing-hans-MacBook-Pro" = withSystem "aarch64-darwin" (ctx:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = ctx.inputs'.nixpkgs.legacyPackages;
+
         modules = [
           # NOTE: any way of making this conciser... ?
           ({ config, pkgs, ... }: {
+            inherit (ctx) nixpkgs;
             imports = [
               (import ./home {
                 inherit config pkgs;
                 dotfiles = "${config.home.homeDirectory}/Projects/kapi-sysconf/users/home/";
+                username = "thing-hanlim";
+                stateVersion = "24.05";
               })
             ];
           })
         ];
-      };
-    };
+      }
+    );
+  };
 }
